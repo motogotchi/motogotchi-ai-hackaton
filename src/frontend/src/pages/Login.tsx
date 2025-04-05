9b5930f91c0f7d1bec4eb264c26c67e2ab7fb998
Kuldar Kalvik <email@kuldar.com>
2025-04-05 17:08:25 +0300

    update

--- src/frontend/src/pages/Login.tsx
+++ src/frontend/src/pages/Login.tsx
@@ -11,7 +11,7 @@ const Login = () => {
         disabled={isLoading}
         className="cursor-pointer m-auto rounded-md bg-indigo-600 px-4 py-2.5 font-clash font-medium text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50 disabled:cursor-not-allowed"
       >
-        {isLoading ? "Logging in..." : "Login with Internet Identity"}
+        {isLoading ? "Logging in..." : "Login with Internet Identity!"}
       </button>
     </div>
   );
